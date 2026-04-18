54c4ef544d11b4a591ccec6b005438e79c590e8a
ChiYuouo <zyc630089289@outlook.com>
2026-04-18 14:07:33 +0800

    店铺列表缓存

--- src/main/java/com/hmdp/utils/RedisConstants.java
+++ src/main/java/com/hmdp/utils/RedisConstants.java
@@ -1,5 +1,7 @@
 package com.hmdp.utils;
 
+import cn.hutool.json.JSONUtil;
+
 public class RedisConstants {
     public static final String LOGIN_CODE_KEY = "login:code:";
     public static final Long LOGIN_CODE_TTL = 2L;
@@ -10,7 +12,7 @@ public class RedisConstants {
 
     public static final Long CACHE_SHOP_TTL = 30L;
     public static final String CACHE_SHOP_KEY = "cache:shop:";
-
+    public static final String CACHE_SHOP_TYPE_KEY = "cache:shop:type:";
     public static final String LOCK_SHOP_KEY = "lock:shop:";
     public static final Long LOCK_SHOP_TTL = 10L;
 
